efeccba0db4b2ae6d1523e18d9367ea2022cf15b
krissgoshevski <krissgoshevski@example.com>
2024-02-02 22:44:45 +0100

    change

--- flask-api-client-script/controllers/ScriptUserController.py
+++ flask-api-client-script/controllers/ScriptUserController.py
@@ -3,10 +3,10 @@ import json
 
 
 
-# API Endpoints
-total_spending_endpoint = "/api/total_spent" # first requirement
-write_eligible_users_endpoint = "/api/write_eligible_users"
-average_spending_by_age_endpoint = "/api/average_spending_by_age"
-
-
-
+# # API Endpoints
+# total_spending_endpoint = "/api/total_spent" # first requirement
+# write_eligible_users_endpoint = "/api/write_eligible_users"
+# average_spending_by_age_endpoint = "/api/average_spending_by_age"
+#
+#
+#
