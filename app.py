45d98f83dddbad8f662389afc6ce0ab3f95400c3
krissgoshevski <krissgoshevski@example.com>
2024-02-07 22:17:00 +0100

    change

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask
-from config import ConfigMysql, ConfigMongoDB
+from config import ConfigMysql
 from models.UserInfo import db, UserSpending, UserInfo
 from flask_migrate import Migrate
 from Seeders.UserSeeder import UserSeeder
 from routes.api_routes import api_routes
-from pymongo import MongoClient
-
-
-
 
 
 app = Flask(__name__)
@@ -17,11 +13,6 @@ app.config.from_object(ConfigMysql)
 db.init_app(app)
 migrate = Migrate(app, db)
 
-
-# MongoDB configuration
-app.config.from_object(ConfigMongoDB)
-mongo_client = MongoClient(app.config['MONGO_URI'])
-mongo_db = mongo_client.get_database()
 
 # routes
 app.register_blueprint(api_routes, url_prefix='/api')
