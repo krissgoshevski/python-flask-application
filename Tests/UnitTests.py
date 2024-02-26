928ffdaf45cce6bd2debf41b323c9c360e632de8
krissgoshevski <krissgoshevski@example.com>
2024-02-26 19:40:50 +0100

    change

--- Tests/UnitTests.py
+++ Tests/UnitTests.py
@@ -4,10 +4,8 @@ import os
 from app import app
 from unittest.mock import patch, MagicMock
 import json
-from models.UserInfo import UserInfo, UserSpending
-
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from models.UserInfo import UserInfo, UserSpending, db
+import logging
 
 
 project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
@@ -36,38 +34,91 @@ class TestAPIEndpoints(unittest.TestCase):
                 f"The user with ID {user_id} has not spent money yet. Received status code {response.status_code}")
 
     def test_get_average_spending_by_age(self):
-        # Mock database session and query
-        with patch('flask_sqlalchemy.SQLAlchemy.session') as mock_session:
-            mock_query = MagicMock()
-            mock_query.scalar.side_effect = [150.0, 262.5, 0.0, 3367103.3, 3563.3]
-            mock_session.return_value.query.return_value = mock_query
+        try:
+            # Mock database session and query
+            with patch('app.db.session') as mock_session:
+                mock_query = MagicMock()
+                mock_query.scalar.side_effect = [150.0, 262.5, 0.0, 3367103.3, 3563.3]
+                mock_session.query.return_value = mock_query
 
-            response = self.app.get('/api/average_spending_by_age')
-        data = json.loads(response.data)
-        self.assertEqual(response.status_code, 200)
+                response = self.app.get('/api/average_spending_by_age')
+            data = json.loads(response.data)
+            self.assertEqual(response.status_code, 200)
 
-        # Assert expected average spending values
-        self.assertEqual(data['18-24'], 150.0)
-        self.assertEqual(data['25-30'], 262.5)
-        self.assertEqual(data['31-36'], 0.0)
-        self.assertEqual(data['37-47'], 3367103.3)
-        self.assertEqual(data['48-99'], 3563.3)
+            # Assert expected average spending values
+            expected_data = {
+                "18-24": 150.0,
+                "25-30": 262.5,
+                "31-36": 0.0,
+                "37-47": 3367103.3,
+                "48-99": 3563.3
+            }
+            self.assertEqual(data, expected_data)
+
+        except Exception as e:
+            logging.error(f"Error in test_get_average_spending_by_age: {str(e)}")
+            self.fail("An unexpected error occurred during the test.")
+
+        finally:
+            db.session.close()
 
     def test_get_total_spending_by_users(self):
         response = self.app.get('/api/total_spending_users')
         data = json.loads(response.data)
+
+        # Assert response status code
         self.assertEqual(response.status_code, 200)
+
+        # Assert response data
+        expected_data = {"total_spending_users": 10113350.0}
+        self.assertEqual(data, expected_data)
 
     def test_get_users_with_vouchers(self):
         response = self.app.get('/api/users/vouchers')
         data = json.loads(response.data)
+
+        # Assert response status code
         self.assertEqual(response.status_code, 200)
 
+        # Assert response data
+        expected_data = {
+            "users_for_voucher": [
+                {
+                    "age": 47,
+                    "email": "amy59@example.com",
+                    "name": "Katherine Porter",
+                    "user_id": 3
+                },
+                {
+                    "age": 95,
+                    "email": "mitchelljeremy@example.net",
+                    "name": "random user",
+                    "user_id": 17
+                }
+            ]
+        }
+        self.assertEqual(data, expected_data)
 
     def test_total_spending_above_1000(self):
         response = self.app.get('/api/total_spending_above_1000')
         data = json.loads(response.data)
+
+        # Assert response status code
         self.assertEqual(response.status_code, 200)
+
+        # Assert response data
+        expected_data = [
+            {
+                "total_spending": 10090.0,
+                "user_id": 17
+            },
+            {
+                "total_spending": 10101000.0,
+                "user_id": 3
+            }
+        ]
+        self.assertEqual(data, expected_data)
+
 
 if __name__ == '__main__':
     unittest.main()
